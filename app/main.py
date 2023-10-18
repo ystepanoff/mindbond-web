f7d367ad9dcb1c74ce84ec95aae517a84d6d7666
Yegor Stepanov <ystepanoff@icloud.com>
2023-10-18 15:22:04 +0100

    Add navbar on the main page

--- app/main.py
+++ app/main.py
@@ -15,7 +15,6 @@ from flask import (
 def validate_user(user_id: int, user_token: str) -> Optional[Dict[str, Any]]:
     response = requests.post(current_app.get_service_url('validate'), json={'token': user_token})
     payload = json.loads(response.text)
-    print(payload)
     if payload['status'] == http.HTTPStatus.OK and payload['userId'] == user_id:
         return {
             'id': payload['userId'],
