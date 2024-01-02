93a1e64b4f2a5afdfe0999dadb9dddc00c5e24c9
Yegor Stepanov <ystepanoff@icloud.com>
2024-01-02 19:40:56 +0000

    Draft fixint the message input field

--- app/static/chats.js
+++ app/static/chats.js
@@ -1,12 +1,129 @@
 let sendMessage = (userFromId, userToId, message) => {
-    console.log(userFromId, userToId, message);
+    let response = requestEndpoint("/chat/send", {
+        "userFromId": userFromId,
+        "userToId": userToId,
+        "message": message
+    });
+    console.log(response);
 }
 
 let loadChat = (contact) => {
     let userId = parseInt(getCookie("_id"));
 
+    let chatContentDiv = document.getElementById('chatContentDiv');
+    chatContentDiv.innerHTML = '';
+
+
+    // TODO: create the following structure:
+    // <div className="msg-head">
+    //     <div className="row">
+    //         <div className="col-8">
+    //             <div className="d-flex align-items-center">
+    //                 <div className="flex-shrink-0">
+    //                     <img className="img-fluid"
+    //                          src="{{ url_for('static', filename='images/user.png') }}"
+    //                          alt="user img">
+    //                 </div>
+    //                 <div id="chatContactText" className="flex-grow-1 ms-3">
+    //                     <h3>Pasha Celluloid</h3>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //         <div className="col-4">
+    //             <ul className="moreoption">
+    //                 <li className="navbar nav-item dropdown">
+    //                     <a className="nav-link dropdown-toggle" href="#" role="button"
+    //                        data-bs-toggle="dropdown" aria-expanded="false"><i
+    //                         className="fa fa-ellipsis-v" aria-hidden="true"></i></a>
+    //                     <ul className="dropdown-menu">
+    //                         <li><a className="dropdown-item" href="#">Action</a></li>
+    //                         <li><a className="dropdown-item" href="#">Another action</a></li>
+    //                         <li>
+    //                             <hr className="dropdown-divider">
+    //                         </li>
+    //                         <li><a className="dropdown-item" href="#">Something else here</a>
+    //                         </li>
+    //                     </ul>
+    //                 </li>
+    //             </ul>
+    //         </div>
+    //     </div>
+    // </div>
+    //
+    //
+    // <div className="modal-body">
+    //     <div className="msg-body">
+    //         <ul>
+    //             <li className="message_from">
+    //                 <p> Hey, Are you there? </p>
+    //                 <span className="time">10:06 am</span>
+    //             </li>
+    //             <li className="message_from">
+    //                 <p> Hey, Are you there? </p>
+    //                 <span className="time">10:16 am</span>
+    //             </li>
+    //             <li className="message_to">
+    //                 <p>yes!</p>
+    //                 <span className="time">10:20 am</span>
+    //             </li>
+    //             <li className="message_from">
+    //                 <p> Hey, Are you there? </p>
+    //                 <span className="time">10:26 am</span>
+    //             </li>
+    //             <li className="message_from">
+    //                 <p> Hey, Are you there? </p>
+    //                 <span className="time">10:32 am</span>
+    //             </li>
+    //             <li className="message_to">
+    //                 <p>How are you?</p>
+    //                 <span className="time">10:35 am</span>
+    //             </li>
+    //             <li>
+    //                 <div className="divider">
+    //                     <h6>Today</h6>
+    //                 </div>
+    //             </li>
+    //
+    //             <li className="message_to">
+    //                 <p> yes, tell me</p>
+    //                 <span className="time">10:36 am</span>
+    //             </li>
+    //             <li className="message_to">
+    //                 <p>yes... on it</p>
+    //                 <span className="time">just now</span>
+    //             </li>
+    //
+    //         </ul>
+    //     </div>
+    // </div>
+    //
+    //
+    // <div className="send-box">
+    //     <form>
+    //         <input type="text" id="actionNewMessageInput" className="form-control" aria-label="message…"
+    //                placeholder="Write message…">
+    //
+    //             <button type="button" id="actionNewMessageTrigger"><i className="fa fa-paper-plane"
+    //                                                                   aria-hidden="true"></i> Send
+    //             </button>
+    //     </form>
+    // </div>
+
     let chatContactTextElementTextDiv = document.getElementById("chatContactText");
     chatContactTextElementTextDiv.innerHTML = "<h3>" + contact["handle"] + "</h3>";
+
+    let actionNewMessageInputElement = document.getElementById("actionNewMessageInput");
+    let actionNewMessageTriggerElement = document.getElementById("actionNewMessageTrigger");
+    actionNewMessageInputElement.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            sendMessage(userId, contact["userId"], document.getElementById("actionNewMessageInput").value);
+        }
+        return true;
+    });
+    actionNewMessageTriggerElement.addEventListener("click", () => {
+        sendMessage(userId, contact["userId"], document.getElementById("actionNewMessageInput").value);
+        return true;
+    });
 
     // TODO: handle unapproved contacts
     if (contact["approved"] === false) {
@@ -22,14 +139,7 @@ let loadChat = (contact) => {
         let chatId = data["chatId"];
 
         // TODO: load messages
-        document.getElementById("actionNewMessageInput").addEventListener("keyup", (event) => {
-            if (event.key === "Enter") {
-                sendMessage(userId, contact["userId"], document.getElementById("actionNewMessageInput").value);
-            }
-        });
-        document.getElementById("actionNewMessageTrigger").addEventListener("click", () => {
-            sendMessage(userId, contact["userId"], document.getElementById("actionNewMessageInput").value);;
-        });
+
     }
 
     return null;
@@ -58,7 +168,7 @@ window.onload = () => {
             contactElementImageDiv.innerHTML = "<img class=\"img-fluid\" " +
                 "src=\"/static/images/user.png\" " +
                 "alt=\"user img\">";
-                //"<span class=\"active\"></span>";
+            //"<span class=\"active\"></span>";
             contactElement.appendChild(contactElementImageDiv);
 
             let contactElementTextDiv = document.createElement("div");
